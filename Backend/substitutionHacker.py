49fb1bc1238ff5e96b47d3772dca9bd6adc4f666
Bejai Cobbing <z5313120@ad.unsw.edu.au>
2022-04-13 01:55:09 +1000

    Committing because I actaully got something working!!!!

--- Backend/substitutionHacker.py
+++ Backend/substitutionHacker.py
@@ -2,11 +2,13 @@
 # It uses some code/ideas from https://inventwithpython.com/hacking/chapter18.html
 
 from wordPatternsGeneral import getWordPattern, createAllWordPatternsFile
-import os, re, copy
+from detectEnglish import getPercentageEnglishWords
+
+import os, re, copy, itertools
 
 # We want to import allWordPatterns, but the allWordPatterns.py file might not exist yet
-# if not os.path.exists('allWordPatterns.py'):
-createAllWordPatternsFile()
+if not os.path.exists('allWordPatterns.py'):
+    createAllWordPatternsFile()
 from allWordPatterns import allWordPatterns
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -14,29 +16,37 @@ nonLettersSpaceOrApostrophePattern = re.compile('[^A-Z\s\']')
 
 def substitutionHacker(cipherText):
     intersectedMapping = getIntersectedMapping(cipherText)
-    key = {}
-
-    # Create the key based on the intersected mapping
-    for cipherLetter in LETTERS:
-        if len(intersectedMapping[cipherLetter]) == 1:
-            key[cipherLetter] = intersectedMapping[cipherLetter][0]
-        else:
-            key[cipherLetter] = '_'
-
-    # Decrypt the cipherText using the key
-    plainText = []
-    for character in cipherText:
-        if character in LETTERS or character.upper() in LETTERS:
-            if character in LETTERS:
-                plainText.append(key[character])
-            else:
-                plainText.append(key[character.upper()].lower())
-        else:
-            plainText.append(character)
+    print(intersectedMapping)
     
-    plainText = ''.join(plainText)
-    print(plainText)
-    print(key)
+
+    cipherLetters = intersectedMapping.keys()
+    values = (intersectedMapping[cipherLetter] for cipherLetter in cipherLetters)
+    possibleKeys = [dict(zip(cipherLetters, possibleKey)) for possibleKey in itertools.product(*values)]
+
+    highestPercentageEnglish = 0
+    bestKey = {}
+    
+    for potentialKey in possibleKeys:
+        potentialKeyTranslated = {}
+        for cipherLetter in LETTERS:
+            potentialKeyTranslated[cipherLetter] = potentialKey[cipherLetter][0]
+
+        potentialPlainText = decrypt(cipherText, potentialKeyTranslated)
+        percentageEnglish = getPercentageEnglishWords(potentialPlainText)
+
+        if percentageEnglish > highestPercentageEnglish:
+            highestPercentageEnglish = percentageEnglish
+            bestKey = potentialKeyTranslated
+    
+    return {
+        "plainText": decrypt(cipherText, bestKey),
+        "key": potentialKeyTranslated
+    }
+
+
+
+    
+    
 
 
         
@@ -68,9 +78,12 @@ def getIntersectedMapping(cipherText):
 
         # Intersect the new mapping with the existing intersected mapping
         intersectedMapping = intersectMappings(intersectedMapping, currentWordMapping)
-
+    
     # Remove any solved letters from the other lists.
     removeSolvedLettersFromMapping(intersectedMapping)
+
+    # Convert Blanks
+    convertBlanks(intersectedMapping)
     
     return intersectedMapping
         
@@ -128,9 +141,51 @@ def removeSolvedLettersFromMapping(letterMapping):
                     # If a new letter has now been solved, loop again
                     if len(letterMapping[cipherLetter]) == 1:
                         loopAgain = True
+    return
+
+def convertBlanks(letterMapping):
+    #listOfRemainingLetters = list(LETTERS)
+    #for cipherLetter in LETTERS:
+    #    if len(letterMapping[cipherLetter]) == 1:
+    #        listOfRemainingLetters.remove(letterMapping[cipherLetter][0])
+    
+    
+    for cipherLetter in LETTERS:
+        if letterMapping[cipherLetter] == []:
+            letterMapping[cipherLetter] = ["_"]
+    
+    return
+
+
+def decrypt(cipherText, key):
+    # Decrypt the cipherText using the key
+    plainText = []
+    for character in cipherText:
+        if character in LETTERS or character.upper() in LETTERS:
+            if character in LETTERS:
+                plainText.append(key[character])
+            else:
+                plainText.append(key[character.upper()].lower())
+        else:
+            plainText.append(character)
+    
+    plainText = ''.join(plainText)
+    return plainText
+
+
+
+
+
 
 def main():
-    substitutionHacker("Oybb brkl vsu br usyev iru bvs tbyut, cpb Gsurqwey bvrpjvb wb fyt bvs ortb uwlwepkrpt ylgwes tvs'l sgsu useswgsl. Tpus, wb vyl cssq fskk-osyqwqj fvsq vs tywl wb, cpb tvs lwlq'b pqlsutbyql fvm yqmrqs frpkl fyqb br tpjjstb trosbvwqj bvyb frpkl kwbsuykkm hwkk mrp wi mrp yebpykkm oyqyjsl br yevwsgs wb. Bvsus't qr fym vs'l yjuss.")
+    data = substitutionHacker("""Tvs ythsl bvs npstbwrq sgsq bvrpjv tvs lwlq'b usykkm fyqb br vsyu bvs yqtfsu. Wb fyt y qr-fwq twbpybwrq twqes tvs ykusylm hqsf. Wi vs brkl bvs bupbv, tvs'l jsb erqiwuoybwrq ri vsu frutb isyut. Wi vs kwsl, tvs'l hqrf bvyb vs fytq'b fvr tvs bvrpjvb vs fyt fvwev frpkl cs ykortb yt cyl. Msb tvs ythsl bvs npstbwrq yqmfym yql fywbsl iru vwt yqtfsu.
+
+Trosbwost wb't bvs iwutb orosqb ri bvs lym bvyb eybevst mrp rii jpyul. Bvyb't fvyb Fsqlm fyt bvwqhwqj. Tvs rxsqsl vsu fwqlrf br tss iwus sqjwqst teussevwqj lrfq bvs tbussb. Fvwks bvwt fytq'b trosbvwqj eroxksbskm pqvsyul ri, wb yktr fytq'b qruoyk. Wb fyt y tpus twjq ri fvyb fyt jrwqj br vyxxsq bvyb lym. Tvs erpkl issk wb wq vsu crqst yql wb fytq'b bvs fym tvs fyqbsl bvs lym br csjwq.
+
+Xybuweh lwlq'b fyqb br jr. Bvs iyeb bvyb tvs fyt wqtwtbwqj bvsm optb jr oyls vwo fyqb br jr sgsq kstt. Vs vyl qr lstwus br oyhs toykk bykh fwbv tbuyqjsut vs frpkl qsgsu yjywq tss dptb br cs xrkwbs. Cpb tvs wqtwtbsl bvyb Xybuweh jr, yql tvs frpkl trrq iwql rpb bvyb bvwt frpkl cs bvs cwjjstb owtbyhs tvs erpkl oyhs wq bvswu uskybwrqtvwx.""")
+
+    print(data["plainText"])
+    print(data["key"])
 
 if __name__ == "__main__":
     main()
