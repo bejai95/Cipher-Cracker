8419e2d3778dbeb831f0d6afa460a5a5b0e6a877
Bejai Cobbing <z5313120@ad.unsw.edu.au>
2022-04-14 02:06:53 +1000

    Basically adding a checkpoint

--- Backend/server.py
+++ Backend/server.py
@@ -1,11 +1,29 @@
 from flask import Flask, request
 from json import dumps
+from caesarHacker import caesarHacker
+from transpositionHacker import transpositionHacker
+from substitutionHacker import substitutionHacker
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This route doesn't actually do anything"
+    return "Please use a different route"
 
+@app.route('/caesar', methods=['PUT'])
+def caesar():
+    data = request.get_json()
+    return caesarHacker(data["cipherText"])
+
+@app.route('/transposition', methods=['PUT'])
+def transposition():
+    data = request.get_json()
+    return caesarHacker(data["cipherText"])
+
+@app.route('/substitution', methods=['PUT'])
+def substitution():
+    data = request.get_json()
+    return substitutionHacker(data["cipherText"])
+    
 if __name__ == "__main__":
     app.run(port=0)
