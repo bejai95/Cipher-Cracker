7edd7367425c6bfdac3dc53b3da1994876e246aa
Bejai Cobbing <z5313120@ad.unsw.edu.au>
2022-04-17 06:15:42 +1000

    Added frontend functionality, using as checkpoint

--- Frontend/cipher_cracker/pages/index.js
+++ Frontend/cipher_cracker/pages/index.js
@@ -1,8 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import apiurl from "../utils/apiurl";
+import { useState } from "react";
+
+function NewlineText(props) {
+  const text = props.text;
+  const newText = text.split('\n').map(str => <p>{str}</p>);
+  return newText;
+}
 
 export default function Home() {
+  const [plainText, setPlainText] = useState("");
+  const [key, setKey] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const apiurlFull = apiurl + event.target.CipherType.value
+
+    const res = await fetch(
+      apiurlFull,
+      {
+        body: JSON.stringify({
+          cipherText: event.target.cipherText.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'PUT'
+      }
+    )
+
+    const result = await res.json()
+    setPlainText(result.plainText)
+    setKey(result.key)
+  }
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -10,18 +44,24 @@ export default function Home() {
       </Head>
       <div>
         <h1>Cipher Cracker</h1>
-          <form>
-            <select name="Cipher Type" defaultValue="Caesar">
-              <option value="Caesar">Caesar</option>
-              <option value="Transposition">Transposition</option>
-              <option value="Substitution">Substitution</option>
+          <form onSubmit={handleSubmit}>
+            <select name="CipherType" defaultValue="Caesar">
+              <option value="caesar">Caesar</option>
+              <option value="transposition">Transposition</option>
+              <option value="substitution">Substitution</option>
+              <option value="unknown">Unknown</option>
             </select>
             <br></br>
             <br></br>
-            <textarea name="textValue" placeholder="Write here" rows="20" cols="50" />
+            <textarea name="cipherText" placeholder="Enter Ciphertext..." rows="20" cols="50" />
             <br></br>
             <button type="submit">Submit</button>
           </form>
+          <h2>Plaintext:</h2>
+          <NewlineText text={plainText} />
+          <h2>Key:</h2>
+          <p>{key}</p>
+
       </div>
     </div>
   )
---
+++ Frontend/cipher_cracker/utils/apiurl.js
@@ -0,0 +1,2 @@
+const apiurl = "http://ciphercrackerapi-env.eba-eqyks7ak.ap-southeast-2.elasticbeanstalk.com/";
+export default apiurl;
