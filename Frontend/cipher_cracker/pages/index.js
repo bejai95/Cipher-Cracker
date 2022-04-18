de319933c6c385b9919ae1dd629d45b32a1c25b0
Bejai Cobbing <z5313120@ad.unsw.edu.au>
2022-04-19 08:19:44 +1000

    Got site looking pretty sweet

--- Frontend/cipher_cracker/pages/index.js
+++ Frontend/cipher_cracker/pages/index.js
@@ -2,26 +2,27 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import apiurl from "../utils/apiurl";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import NavBar from "../components/NavBar.js"
-
-function NewlineText(props) {
-  const text = props.text;
-  const newText = text.split('\n').map((str, index) => <p key={index}>{str}</p>);
-  return newText;
-}
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import CloseButton from 'react-bootstrap/CloseButton';
 
 export default function Home() {
   const [alreadyBeenSubmit, setAlreadyBeenSubmit] = useState(false);
+
   const [cipherText, setCipherText] = useState("");
   const [plainText, setPlainText] = useState("");
   const [key, setKey] = useState("");
+
+  const [showErrorModal, setShowErrorModal] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
   const [unknownCipherType, setUnknownCipherType] = useState("");
+  
+  const [showSubstitutionModal, setShowSubstitutionModal] = useState(false);
   const [totalAmountPossibilities, setTotalAmountPossibilities] = useState("");
   const [intersectedMapping, setIntersectedMapping] = useState({});
-  const [isSubstitution, setIsSubstitution] = useState(false);
-
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -32,10 +33,10 @@ export default function Home() {
     const apiurlFull = apiurl + cipherType;
 
     if (cipherType === "substitution") {
-      setIsSubstitution(true)
+      setShowSubstitutionModal(true)
       apiurlFull += "?mode=partial"
     } else {
-      setIsSubstitution(false)
+      setShowSubstitutionModal(false)
     }
 
     const res = await fetch(
@@ -54,6 +55,7 @@ export default function Home() {
     const result = await res.json()
 
     setErrorMessage("")
+    setShowErrorModal(false)
     setUnknownCipherType("")
     setTotalAmountPossibilities("")
     setIntersectedMapping({})
@@ -63,11 +65,19 @@ export default function Home() {
       setPlainText(result.result.plainText)
 
       if (result.cipherType === "substitution") {
-        setIsSubstitution(true)
-        setTotalAmountPossibilities(result.result.totalAmountPossibilities)
+        setShowSubstitutionModal(true)
+        
+        let amountPossible = result.result.totalAmountPossibilities;
+        if (amountPossible != 1) {
+          setTotalAmountPossibilities(amountPossible)
+        } else {
+          setTotalAmountPossibilities("infinite")
+        }
+        
         setIntersectedMapping(result.result.intersectedMapping)
         const keyAsString = JSON.stringify(result.result.key)
-        setKey(keyAsString)
+        setKey(keyAsString);
+
       } else {
         setKey(result.result.key)
       }
@@ -77,11 +87,18 @@ export default function Home() {
         setPlainText("");
         setKey("");
         setErrorMessage(result.message)
+        setShowErrorModal(true)
       } else {
         setPlainText(result.plainText)
         
         if (cipherType === "substitution") {
-          setTotalAmountPossibilities(result.totalAmountPossibilities)
+          let amountPossible = result.totalAmountPossibilities;
+          if (amountPossible != 1) {
+            setTotalAmountPossibilities(amountPossible)
+          } else {
+            setTotalAmountPossibilities("infinite")
+          }
+        
           setIntersectedMapping(result.intersectedMapping)
           const keyAsString = JSON.stringify(result.key)
           setKey(keyAsString)
@@ -92,7 +109,15 @@ export default function Home() {
     }
   }
 
-  async function handleFullSubstitution() {
+  function handleErrorModalClose() {
+    setShowErrorModal(false);
+  }
+
+  function handlePartialSubstitutionDecipher() {
+    setShowSubstitutionModal(false)
+  }
+
+  async function handleFullSubstitutionDecipher() {
     const res = await fetch(
       apiurl + 'substitution?mode=full',
       {
@@ -113,125 +138,116 @@ export default function Home() {
     setKey(keyAsString)
   }
 
+
   return (
     <div>
       <Head>
         <title>Cipher Cracker</title>
       </Head>
       <div className={styles.container}>
-        <NavBar></NavBar>
+        <NavBar/>
 
-
-      <div className='mb-4'>
-        <h1 className="display-4">Decrypter Tool</h1>
-        <p className="lead">Use this tool to decrypt any Caesar, Transposition or Substitution cipher. For more information, please see the Encryption Methods page.</p>
-      </div>
-
-      <div className="d-flex flex-row justify-content-between">
-
-        <div className="jumbotron" style={{width: "40%"}}>
-          <h1 className="display-6">Query</h1>
-          <hr className="my-4"></hr>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="selectCipherType" className="form-label">Cipher Type:</label>
-              <select className="form-select" name="cipherType" defaultValue="caesar" id="selectCipherType">
-                <option value="caesar">Caesar</option>
-                <option value="transposition">Transposition</option>
-                <option value="substitution">Substitution</option>
-                <option value="unknown">Unknown</option>
-              </select>
-              <div className="form-text">If the encryption method of the ciphertext is not known, use the unknown option.</div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="cipherText" className="form-label">Ciphertext:</label>
-              <textarea name="cipherText" id="cipherText" className="form-control" placeholder="Enter Ciphertext..." rows="9" />
-            </div>
-            <button type="submit" className="btn btn-primary btn-lg">Decrypt</button>
-          </form>
+        <div className='mb-4'>
+          <h1 className="display-4">Decrypter Tool</h1>
+          <p className="lead">Use this tool to decrypt any Caesar, Transposition or Substitution cipher. For more information, please see the Encryption Methods page.</p>
         </div>
 
-        <div style={{position: "relative", width: "15%"}}>
-          <Image src="/arrow-right.svg" layout="fill" objectFit="contain"></Image>
-        </div>
+        <div className="d-flex flex-row justify-content-between">
+
+          <div className="jumbotron" style={{width: "41%"}}>
+            <h1 className="display-6">Query</h1>
+            <hr className="my-4"></hr>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="selectCipherType" className="form-label">Cipher Type:</label>
+                <select className="form-select" name="cipherType" defaultValue="caesar" id="selectCipherType">
+                  <option value="caesar">Caesar</option>
+                  <option value="transposition">Transposition</option>
+                  <option value="substitution">Substitution</option>
+                  <option value="unknown">Unknown</option>
+                </select>
+                <div className="form-text">If the encryption method of the ciphertext is not known, use the unknown option.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="cipherText" className="form-label">Ciphertext:</label>
+                <textarea name="cipherText" id="cipherText" className="form-control" placeholder="Enter Ciphertext..." rows="9" />
+              </div>
+              <button type="submit" className="btn btn-primary btn-lg">Decrypt</button>
+            </form>
+          </div>
+
+          <div style={{position: "relative", width: "13%"}}>
+            <Image src="/arrow-right.svg" layout="fill" objectFit="contain" />
+          </div>
+          
+          <div className="jumbotron" style={{width: "41%"}}>
+            <h1 className="display-6">Result</h1>
+            <hr className="my-4"></hr>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="key" className="form-label">Key:</label>
+                <textarea id="key" className="form-control" placeholder="Please enter valid ciphertext first..." value={key} rows="2" readOnly></textarea>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="plaintext" className="form-label">Plaintext:</label>
+                <textarea id="plaintext" className="form-control" placeholder="Please enter valid ciphertext first..." value={plainText} rows="9" readOnly/>
+              </div>
+              {unknownCipherType &&
+                <p> The plaintext for the ciphertext that you entered was encrypted using a <b> {unknownCipherType} </b> cipher. </p>
+              }       
+            </form>
+          </div>
         
-
-        <div className="jumbotron" style={{width: "40%"}}>
-          <h1 className="display-6">Result</h1>
-          <hr className="my-4"></hr>
-
-          <form>
-            <div className="mb-3">
-            <label htmlFor="key" className="form-label">Key:</label>
-            <textarea id="key" className="form-control" placeholder="Please enter valid ciphertext first..." value={key} rows="2" readOnly></textarea>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="plaintext" className="form-label">Plaintext:</label>
-              <textarea id="plaintext" className="form-control" placeholder="Please enter valid ciphertext first..." value={plainText} rows="9" readOnly/>
-            </div>
-          </form>
-          
-          
-          
-
-          
-          
-
-
-          
-
-          {alreadyBeenSubmit &&
-            <div id='results-box'>
-              {errorMessage ? (
-                <div id='error-message-box'>
-                  <h2>Error Message:</h2>
-                  <p>{errorMessage}</p>
-                </div>
-              ) : ( 
-                
-                <div id='general-results-box'>
-                  {unknownCipherType &&
-                    <p> The plaintext for the ciphertext that you entered was encrypted using a <b>{unknownCipherType}</b> cipher. </p>
-                  }                  
-                  
-                  <h2>Plaintext:</h2>
-                  <NewlineText text={plainText} />
-                  <h2>Key:</h2>
-                  <p>{key}</p>
-
-                  {isSubstitution &&
-                    <div id='full-substitution-options-box'>
-                      <h2>Total Amount Possibilities (for substitution):</h2>
-                      <p>{totalAmountPossibilities}</p>
-                      <h2>Full substitution option:</h2>
-                      <button onClick={handleFullSubstitution}>Full Substitution</button>
-                    </div>
-                  }
-                
-                </div>
-              )} 
-            </div>
-          }
-          
         </div>
 
+        <Modal 
+          show={showErrorModal}
+          backdrop="static"
+          keyboard={false}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header>
+            <Modal.Title>Error</Modal.Title>
+            <CloseButton onClick={handleErrorModalClose}/>
+          </Modal.Header>
+          <Modal.Body>{errorMessage}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleErrorModalClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
 
-      </div>
-
-      
-      
-      
-      
-      
-
-        
-
-        
-
-
-
-
-        
+        <Modal 
+          show={showSubstitutionModal}
+          backdrop="static"
+          keyboard={false}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+        >
+          <Modal.Header>
+            <Modal.Title>Substitution Decryption Options</Modal.Title>
+            <CloseButton onClick={handlePartialSubstitutionDecipher}/>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Doing a partial substitution decipher, the decrypter tool initially found {<b>{totalAmountPossibilities}</b>} possible keys for your ciphertext. You can choose to either view the plaintext using the currently known letters from the partial decipher (with _'s in the place of the unknown letters), or do a full decipher to potentially decrypt the remaining letters. </p>
+            <p><b>Approximate Response Times for Full Decipher:</b></p>
+            <ul>
+              <li> {"<"} 1000 possible keys: 1-3 seconds</li>
+            </ul>
+          </Modal.Body>
+          <Modal.Footer>
+          <Button variant="primary" onClick={handlePartialSubstitutionDecipher}>
+            Partial Decipher
+          </Button>
+          <Button variant="success" onClick={handleFullSubstitutionDecipher}>
+            Full Decipher
+          </Button>
+          </Modal.Footer>
+        </Modal>
+            
       </div>
     </div>
   )
